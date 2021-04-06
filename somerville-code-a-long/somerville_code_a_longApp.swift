9af889f890a5bbef9ac38d02273ba8d87283cc60
Alasdair West <alasdair.west@and.digital>
2021-04-06 17:39:12 +0100

    Initial Commit

---
+++ somerville-code-a-long/somerville_code_a_longApp.swift
@@ -0,0 +1,17 @@
+//
+//  somerville_code_a_longApp.swift
+//  somerville-code-a-long
+//
+//  Created by Alasdair West on 06/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct somerville_code_a_longApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
